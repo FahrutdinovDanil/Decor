7c97389409c8d41b93f6f70956b7e115159d1147
Даниль Фахрутдинов <danilfahrutdinov357@mail.ru>
2023-02-28 18:06:45 +0300

    Добавьте файлы проекта.

---
+++ Decor/DataBase/DataAccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decor.DataBase
+{
+    public class DataAccess
+    {
+        public delegate void RefreshListDelegate();
+        public static event RefreshListDelegate RefreshList;
+        public static List<Product> GetProducts()
+        {
+            return DecorEntities.GetContext().Products.ToList();
+        }
+    }
+}
---
+++ Decor/DataBase/DecorEntities.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decor.DataBase
+{
+    public partial class DecorEntities
+    {
+        private static DecorEntities context;
+        public static DecorEntities GetContext()
+        {
+            if (context == null)
+                context = new DecorEntities();
+
+            return context;
+        }
+    }
+}
